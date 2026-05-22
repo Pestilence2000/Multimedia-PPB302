0af4f9d97e49fb5ee7edc088cc27b8daea2e7f3a
nadja <naso0004@student.hv.se>
2026-05-22 17:32:05 +0200

    Changes to get the database to work

--- Multimedia-app/Controllers/HomeController.cs
+++ Multimedia-app/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Multimedia_app.Models;
 
 namespace Multimedia_app.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ArtDbContext _context;
+
+    public HomeController(ArtDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var artworks = _context.Artworks.ToList();
+        return View(artworks);
     }
 }
